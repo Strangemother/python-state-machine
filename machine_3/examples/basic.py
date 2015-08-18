93d1ac7e599d5236ad213d17b279ee45abb2bd9a
Jay Jagpal <jay@strangemother.com>
2015-08-18 20:47:01 +0100

    Basic example
    Moved the basic usage out of the runner into an example file.

---
+++ machine_3/examples/basic.py
@@ -0,0 +1,65 @@
+from machine import Machine
+from node import Node
+from conditions import Condition
+
+
+class TestNode(Node):
+    color = 'red'
+    age = 22
+    food = 'cherry'
+    bar = 2
+
+    def heard_food(self, node, value, field):
+        print 'I HEARD THAT'
+
+    def conditions(self):
+        C = Condition
+        return (
+            C('TestReactNode', 'heard_food', True, self.heard_food),
+        )
+
+class TestReactNode(Node):
+    heard_food = False
+
+    def node_age(self, node, value, field):
+        print '! ', self.get_name(), 'AGE is 3'
+
+    def node_age_changed(self, node, value, field):
+        print '! ', self.get_name(), 'CHANGE', field, value
+        # print 'Condition', node, field, value
+
+    def node_food_changed(self, node, value, field):
+        print "> ", self.get_name(), 'FOOD', field, value
+        self.heard_food = True
+
+    def conditions(self):
+        C = Condition
+        return (
+            C('TestNode', 'age', 3, self.node_age),
+            C('TestNode', 'age', C.CHANGED, self.node_age_changed),
+            C('TestNode', 'food', C.CHANGED, self.node_food_changed)
+        )
+
+
+def run():
+    print 'machine example'
+    ma = Machine('example')
+    n = TestNode()
+    n2 = TestReactNode()
+    ma.add(n)
+    ma.add(n2)
+    print 'change age on first node'
+    n.age = 7
+    n.age = 3
+    n.age += 3
+    n.color = 'green'
+    n.food = 'Noodles!'
+    print 'done'
+    return ma
+
+def main():
+    return run()
+
+
+if __name__ == '__main__':
+    main()
