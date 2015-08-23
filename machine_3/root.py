45369dbe000d53d296b4a2e8a68dcc8de96c7a16
Jay Jagpal <jay@strangemother.com>
2015-08-23 19:17:34 +0100

    write to sys tool for adding root to the sys.path

--- machine_3/root.py
+++ machine_3/root.py
@@ -1,3 +1,19 @@
 from machine import *
 from node import *
 from conditions import *
+from tools import *
+
+import sys, os
+
+
+def write_sys(path=None):
+    '''
+    Append the directory to the current sys path to ensure
+    loading deep modules can still import root components.
+
+    This method is run from init or main
+    '''
+    p = __file__ if path is None else path
+    d = os.path.dirname( os.path.abspath(p))
+    print 'write_sys', d
+    sys.path.insert(0, d)
