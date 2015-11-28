d32a8c2c24ec8d5b1721fc26760e5a01dbb40d5d
Jay Jagpal <jay@strangemother.com>
2015-11-28 01:30:57 +0000

    created bridge mixin

--- scatter/bridge.py
+++ scatter/bridge.py
@@ -58,40 +58,41 @@ class MethodPointer(object):
 
 class Bridge(MethodPointer):
     '''
-    A Bridge is presented on the network for other Machines to call. 
+    A Bridge is presented on the network for other Machines to call.
     The RPC layer will expose an instance of this class as methods
-    to call. 
-    All methods are relative to the host Machine. 
+    to call.
+    All methods are relative to the host Machine.
     '''
     parent = None
 
-    def __init__(self, parent=None):
+    def get_parent(self):
+        if self.parent is not None:
+            return self.parent
+        return None
+
+    def __init__(self, parent=None, node_event=None):
         if parent:
             self.parent = parent
+            self.name = self.parent.name
+        self._event_handler = node_event
+        print 'bridge node event', node_event
         super(Bridge, self).__init__()
 
-    def foo(self):
-        print 'foo'
-
     def name(self):
-        return self.parent.name
+        return self.name
 
     def add_peer(self, name, uri):
         print 'received peer', name, uri
-        self.parent.add(name, uri)
+        parent = self.get_parent()
+        parent.add(name, uri)
         return True
-
-    def peers(self):
-        '''
-        return a list of peers assigned to this bridge.
-        '''
-        return self.parent.connections
 
     def info(self):
         '''
         Return information about this runnning machine instance.
         '''
-        n = self.parent.name
+
+        n = self.name
         st = self._start_time
         print 'someone asked for information'
         return {
@@ -100,5 +101,44 @@ class Bridge(MethodPointer):
         }
 
     def node_event(self, event, *args, **kw):
-        return self.parent.node_event(event, *args, **kw)
+        print 'bridge node_event', event, args, kw
+        if self._event_handler is not None:
+            print 'calling bridge node_event handler', event, args, kw
+            self._event_handler(event, *args, **kw)
+        parent = self.get_parent()
+        if parent is not None:
+            return parent.node_event(event, *args, **kw)
 
+
+class BridgeMixin(object):
+    '''
+    A Mixin to assist with handling a Bridge and its event.
+    '''
+
+    def make_bridge_manager(self, Manager_class=None, event_handler=None):
+        event_handler = event_handler or self._bridge_event
+        return Manager_class(event_handler)
+
+    def _bridge_event(self, manager, event, *args, **kw):
+        print '\nbridge manager saw event', event, args, kw
+        if event == 'integrate':
+            item.add_peer(self.name, self.bridge)
+
+    def make_bridge(self):
+        return Bridge(self, self._bridge_node_event)
+
+    def _bridge_node_event(self, event, *args, **kw):
+        '''
+        A peer has called the bridge.
+        '''
+        originator = kw.get('machine', None)
+        originator_node = kw.get('node', None)
+        if 'node' in kw:
+            del kw['node']
+        # if 'machine' in kw:
+        #     del kw['machine']
+        print '_bridge_node_event', self, event, args, kw
+        if event == 'set':
+            self.node_set_event(args[0], args[1], args[2],
+                                node=originator_node,
+                                machine=originator)
